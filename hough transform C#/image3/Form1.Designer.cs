9828a4f0842aee94677194e6517c58c07ae7f299
yanjiunhaha <jiun113241@gmail.com>
2018-06-10 16:20:14 +0800

    hough

--- hough transform C#/image3/Form1.Designer.cs
+++ hough transform C#/image3/Form1.Designer.cs
@@ -36,6 +36,10 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -118,15 +122,57 @@
             this.pictureBox4.Location = new System.Drawing.Point(419, 371);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(400, 300);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 7;
             this.pictureBox4.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 14F);
+            this.label3.Location = new System.Drawing.Point(202, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 19);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "邊閥值 : ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(281, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(26, 22);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "200";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(412, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(26, 22);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Text = "100";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 14F);
+            this.label4.Location = new System.Drawing.Point(333, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 19);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "線閥值 : ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(901, 673);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
@@ -156,6 +202,10 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- hough transform C#/image3/Form1.cs
+++ hough transform C#/image3/Form1.cs
@@ -48,6 +48,37 @@ namespace image3
             return g;
         }
 
+        private Bitmap Get24bits(Bitmap im)
+        {
+            Rectangle r1 = new Rectangle(0, 0, im.Width, im.Height);
+            BitmapData d1 = im.LockBits(r1, ImageLockMode.ReadOnly, im.PixelFormat);
+            IntPtr ptr1 = d1.Scan0;
+            byte[] rgb1 = new byte[d1.Stride * im.Height];
+            int skip1 = d1.Stride - im.Width;
+            System.Runtime.InteropServices.Marshal.Copy(ptr1, rgb1, 0, rgb1.Length);
+            im.UnlockBits(d1);
+
+            Bitmap o = new Bitmap(im.Width, im.Height, PixelFormat.Format24bppRgb);
+            Rectangle r2 = new Rectangle(0, 0, o.Width, o.Height);
+            BitmapData d2 = o.LockBits(r2, ImageLockMode.ReadWrite, o.PixelFormat);
+            IntPtr ptr2 = d2.Scan0;
+            byte[] rgb2 = new byte[d2.Stride * o.Height];
+            int skip2 = d2.Stride - o.Width*3;
+            
+            for(int y = 0, counter1 = 0, counter2 = 0; y < im.Height; ++y, counter1 += skip1, counter2 += skip2)
+            {
+                for(int x = 0; x < im.Width; ++x, counter1 += 1, counter2 += 3)
+                {
+                    rgb2[counter2] = rgb2[counter2 + 1] = rgb2[counter2 + 2] = rgb1[counter1];
+                }
+            }
+
+            System.Runtime.InteropServices.Marshal.Copy(rgb2, 0, ptr2, rgb2.Length);
+            o.UnlockBits(d2);
+            
+            return o;
+        }
+
         Bitmap imageRes, image1;
         private void button1_Click(object sender, EventArgs e)
         {
@@ -73,8 +104,9 @@ namespace image3
                             } else
                             {
                                 image1 = imageRes;
+                                imageRes = Get24bits(image1);
                             }
-                            pictureBox1.Image = image1;
+                            pictureBox1.Image = imageRes;
                         }
                     }
                 }
@@ -85,49 +117,50 @@ namespace image3
             }
         }
         
-        private Bitmap Convolve(Bitmap im, int[] mask, int maskSize)
+        private Bitmap Convolve(Bitmap im, int[] mask, int maskSize,int threshold=0)
         {
-            Rectangle r1 = new Rectangle(0, 0, im.Width, im.Height);
-            BitmapData d1 = im.LockBits(r1, ImageLockMode.ReadOnly, im.PixelFormat);
-            IntPtr ptr1 = d1.Scan0;
-            byte[] rgb1 = new byte[d1.Stride * im.Height];
-            int skip = d1.Stride - im.Width;
-            System.Runtime.InteropServices.Marshal.Copy(ptr1, rgb1, 0, rgb1.Length);
-            im.UnlockBits(d1);
+             Rectangle r1 = new Rectangle(0, 0, im.Width, im.Height);
+             BitmapData d1 = im.LockBits(r1, ImageLockMode.ReadOnly, im.PixelFormat);
+             IntPtr ptr1 = d1.Scan0;
+             byte[] rgb1 = new byte[d1.Stride * im.Height];
+             int skip = d1.Stride - im.Width;
+             System.Runtime.InteropServices.Marshal.Copy(ptr1, rgb1, 0, rgb1.Length);
+             im.UnlockBits(d1);
 
-            Bitmap o = new Bitmap(im.Width, im.Height, PixelFormat.Format8bppIndexed);
-            Rectangle r2 = new Rectangle(0, 0, o.Width, o.Height);
-            BitmapData d2 = o.LockBits(r2, ImageLockMode.ReadWrite, o.PixelFormat);
-            IntPtr ptr2 = d2.Scan0;
-            byte[] rgb2 = new byte[d2.Stride * o.Height];
-            
-            for (int y = 0, counter = 0; y < o.Height; ++y, counter += skip)
-            {
-                for (int x = 0; x < o.Width ; ++x, ++counter)
-                {
-                    double sum = 0;
-                    if (x > maskSize / 2 && x < (o.Width - maskSize / 2) && y > maskSize / 2 && y < (o.Height - maskSize / 2))
-                    {
+             Bitmap o = new Bitmap(im.Width, im.Height, PixelFormat.Format8bppIndexed);
+             Rectangle r2 = new Rectangle(0, 0, o.Width, o.Height);
+             BitmapData d2 = o.LockBits(r2, ImageLockMode.ReadWrite, o.PixelFormat);
+             IntPtr ptr2 = d2.Scan0;
+             byte[] rgb2 = new byte[d2.Stride * o.Height];
 
-                        for (int i = -maskSize / 2; i <= maskSize / 2; ++i)
-                        {
-                            for (int j = -maskSize / 2; j <= maskSize / 2; ++j)
-                            {
-                                sum += rgb1[counter + i * d2.Stride + j] * mask[maskSize * (i + maskSize / 2) + j + maskSize / 2];
-                            }
-                        }
-                        sum = (sum < 0) ? 0- sum : sum;
-                        sum = (sum > 255) ? 255 : sum;
-                        rgb2[counter] = Convert.ToByte(sum);
-                    }
-                }
-            }
-            System.Runtime.InteropServices.Marshal.Copy(rgb2, 0, ptr2, rgb2.Length);
-            ColorPalette myPalette = o.Palette;
-            for (int i = 0; i < 256; ++i) myPalette.Entries[i] = Color.FromArgb(i, i, i);
-            o.Palette = myPalette;
-            o.UnlockBits(d2);
-            return o;
+             for (int y = 0, counter = 0; y < o.Height; ++y, counter += skip)
+             {
+                 for (int x = 0; x < o.Width; ++x, ++counter)
+                 {
+                     double sum = 0;
+                     if (x > maskSize / 2 && x < (o.Width - maskSize / 2) && y > maskSize / 2 && y < (o.Height - maskSize / 2))
+                     {
+
+                         for (int i = -maskSize / 2; i <= maskSize / 2; ++i)
+                         {
+                             for (int j = -maskSize / 2; j <= maskSize / 2; ++j)
+                             {
+                                 sum += rgb1[counter + i * d2.Stride + j] * mask[maskSize * (i + maskSize / 2) + j + maskSize / 2];
+                             }
+                         }
+                         sum = (sum < 0) ? 0 - sum : sum;
+                         sum = (sum > 255) ? 255 : sum;
+                         sum = (sum>=threshold)?sum: 0;
+                         rgb2[counter] = Convert.ToByte(sum);
+                     }
+                 }
+             }
+             System.Runtime.InteropServices.Marshal.Copy(rgb2, 0, ptr2, rgb2.Length);
+             ColorPalette myPalette = o.Palette;
+             for (int i = 0; i < 256; ++i) myPalette.Entries[i] = Color.FromArgb(i, i, i);
+             o.Palette = myPalette;
+             o.UnlockBits(d2);
+             return o;
         }
 
         private Bitmap Add(Bitmap ima,Bitmap imb)
@@ -170,25 +203,134 @@ namespace image3
             return o;
         }
 
+        private Bitmap Hough(Bitmap im,int threshold) {
+            Rectangle r1 = new Rectangle(0, 0, im.Width, im.Height);
+            BitmapData d1 = im.LockBits(r1, ImageLockMode.ReadOnly, im.PixelFormat);
+            IntPtr ptr1 = d1.Scan0;
+            byte[] rgb1 = new byte[d1.Stride * im.Height];
+            int skip1 = d1.Stride - im.Width;
+            System.Runtime.InteropServices.Marshal.Copy(ptr1, rgb1, 0, rgb1.Length);
+            im.UnlockBits(d1);
+
+            double D = Math.Sqrt(Math.Pow(im.Width, 2) + Math.Pow(im.Height, 2));
+            int iD = ((int)D < D) ? (int)D + 1:(int)D; 
+            Bitmap o = new Bitmap(180,iD*2,PixelFormat.Format8bppIndexed);
+            ColorPalette myPalette = o.Palette;
+            for (int i = 0; i < 256; ++i) myPalette.Entries[i]=Color.FromArgb(i,i,i);
+            o.Palette = myPalette;
+
+            Rectangle r2 = new Rectangle(0, 0, o.Width, o.Height);
+            BitmapData d2 = o.LockBits(r2, ImageLockMode.ReadWrite, o.PixelFormat);
+            IntPtr ptr2 = d2.Scan0;
+            byte[] rgb2 = new byte[d2.Stride * o.Height];
+            int skip2 = d2.Stride - o.Width;
+            System.Runtime.InteropServices.Marshal.Copy(ptr2, rgb2, 0, rgb2.Length);
+
+            int[] Irgb2 = new int[d2.Stride * o.Height];
+             
+            for (int y = 0, counter = 0; y < im.Height; ++y, counter += skip1)
+            {
+                for(int x = 0; x < im.Width; ++x, ++counter)
+                {
+                    if (rgb1[counter] > 0)
+                    {
+                        for(double theta = -90; theta <= 90; ++theta)
+                        {
+                            ++Irgb2[Convert.ToInt32(x * Math.Cos(theta / 180 * Math.PI) + y * Math.Sin(theta / 180 * Math.PI)+iD) * d2.Stride + (90 + Convert.ToInt32(theta))];
+                        }
+                    }
+                }
+            }
+            int max = 0, min = 255;
+            foreach(int x in Irgb2)
+            {
+                if (x > max) max = x;
+                if (x < min) min = x;
+            }
+            if (max > 255) {
+                double ratio =255.0/(max-min);
+                for (int y = 0, counter = 0; y < o.Height; ++y, counter += skip2)
+                {
+                    for(int x = 0; x < o.Width; ++x, ++counter)
+                    {
+                        Irgb2[counter] = Convert.ToInt32((Irgb2[counter] - min) * ratio);
+                    }
+                }
+            }
+
+            for (int i = 0; i < Irgb2.Length; ++i) rgb2[i] = Convert.ToByte(Irgb2[i]);
+            System.Runtime.InteropServices.Marshal.Copy(rgb2, 0, ptr2, rgb2.Length);
+            o.UnlockBits(d2);
+
+            Rectangle r3 = new Rectangle(0, 0, imageRes.Width, imageRes.Height);
+            BitmapData d3 = imageRes.LockBits(r3, ImageLockMode.ReadWrite, imageRes.PixelFormat);
+            IntPtr ptr3 = d3.Scan0;
+            byte[] rgb3 = new byte[d3.Stride * imageRes.Height];
+            int skip3 = d3.Stride - imageRes.Width * 3;
+            System.Runtime.InteropServices.Marshal.Copy(ptr3, rgb3, 0, rgb3.Length);
+
+            for (int y = 0, counter = 0; y < o.Height; ++y, counter += skip2)
+            {
+                for (int x = 0; x < o.Width; ++x, counter += 1)
+                {
+                    if (Irgb2[counter] > threshold)
+                    {
+                        for (int i = 0, j; i < imageRes.Width; ++i)
+                        {
+                            double temp = ((Convert.ToDouble(y)-iD - Convert.ToDouble(i) * Math.Cos(Convert.ToDouble(x - 90) / 180 * Math.PI)) / Math.Sin(Convert.ToDouble(x - 90) / 180 * Math.PI));
+                            if (temp > 0 && temp < imageRes.Height-1)
+                            {
+                                j = Convert.ToInt32(temp);
+                                try
+                                {
+                                    rgb3[j * d3.Stride + i * 3] = rgb3[j * d3.Stride + i * 3 + 1] = 0;
+                                    rgb3[j * d3.Stride + i * 3 + 2] = 255;
+                                }
+                                catch { MessageBox.Show("x=" + i.ToString() + " y=" + j.ToString()); }
+                            }
+
+                        }
+                    }
+                }
+            }
+            System.Runtime.InteropServices.Marshal.Copy(rgb3, 0, ptr3, rgb3.Length);
+            imageRes.UnlockBits(d3);
+
+            return o;
+        }
+
         private void button2_Click(object sender, EventArgs e)
         {
-            int[] test = { 1, 1, 1, 1, 1, 1, 1, 1, 1 };
-            int[] maskH = { -1, -2, -1, 0, 0, 0, 1, 2, 1 };
-            int[] maskV = { -1, 0, 1, -2, 0, 2, -1, 0, 1 };
-            int[] maskR = { 0, 1, 1, -1, 0, 1, -1, -1, 0 };
-            int[] maskL = { -1, -1, 0, -1, 0, 1, 0, 1, 1 };
-            int maskSize = 3;
-            Bitmap sobelV = Convolve(image1, maskV, maskSize);
-            Bitmap sobelH = Convolve(image1, maskH, maskSize);
-            Bitmap sobel90 = Add(sobelV, sobelH);
+            try {
+                int threshold=Convert.ToInt32(textBox1.Text);
+                int lineThr = Convert.ToInt32(textBox2.Text);
 
-            Bitmap sobelR = Convolve(image1, maskR, maskSize);
-            Bitmap sobelL = Convolve(image1, maskL, maskSize);
-            Bitmap sobel45 = Add(sobelR, sobelL);
-            pictureBox2.Image = sobel45;
-            pictureBox3.Image = sobel90;
-            Bitmap sobel = Add(sobel45, sobel90);
-            pictureBox4.Image = sobel;
+                if (threshold < 0) { MessageBox.Show("Threshold need bigger than 0");return; }
+                else if (threshold > 255) { MessageBox.Show("Threshold need smaller than 255");return; }
+                
+                int[] maskH = { -1, -2, -1, 0, 0, 0, 1, 2, 1 };
+                int[] maskV = { -1, 0, 1, -2, 0, 2, -1, 0, 1 };
+                int[] maskR = { 0, 1, 1, -1, 0, 1, -1, -1, 0 };
+                int[] maskL = { -1, -1, 0, -1, 0, 1, 0, 1, 1 };
+                int maskSize = 3;
+                Bitmap sobelV = Convolve(image1, maskV, maskSize,threshold);
+                Bitmap sobelH = Convolve(image1, maskH, maskSize,threshold);
+                Bitmap sobel90 = Add(sobelV, sobelH);
+
+                Bitmap sobelR = Convolve(image1, maskR, maskSize,threshold);
+                Bitmap sobelL = Convolve(image1, maskL, maskSize,threshold);
+                Bitmap sobel45 = Add(sobelR, sobelL);
+                Bitmap sobel = Add(sobel45, sobel90);
+                pictureBox3.Image = sobel;
+
+                Bitmap test = Hough(sobel,lineThr);
+                pictureBox4.Image = test;
+                pictureBox2.Image = imageRes;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:" + ex.Message);
+            }
         }
     }
 }
