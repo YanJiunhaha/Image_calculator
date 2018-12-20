1c3ab813766771de6db825a358626d732aff5ba3
YanJiun <jiun113241@gmail.com>
2018-12-20 14:45:52 +0800

    updata

---
+++ morphological operations C#/image4/image4/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace image4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "開啟檔案";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Dilation";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 14F);
+            this.label1.Location = new System.Drawing.Point(574, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "MaskSize :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(671, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(18, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 14F);
+            this.label2.Location = new System.Drawing.Point(13, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Input :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 14F);
+            this.label3.Location = new System.Drawing.Point(419, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Output :";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 66);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(423, 66);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(400, 300);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(176, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Erosion";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(257, 13);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Opening";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(338, 13);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Closing";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 376);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("新細明體", 9F);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ morphological operations C#/image4/image4/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace image4
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        enum yan : int
+        {
+            EROSION = 0,
+            DILATION
+        }
+        private Bitmap GetGray(Bitmap im)
+        {
+            Rectangle r1 = new Rectangle(0, 0, im.Width, im.Height);
+            BitmapData d1 = im.LockBits(r1, ImageLockMode.ReadOnly, im.PixelFormat);
+            IntPtr ptr1 = d1.Scan0;
+            byte[] rgb1 = new byte[d1.Stride * im.Height];
+            int skip1 = d1.Stride - im.Width * 3;
+            System.Runtime.InteropServices.Marshal.Copy(ptr1, rgb1, 0, rgb1.Length);
+            im.UnlockBits(d1);
+
+            Bitmap g = new Bitmap(im.Width, im.Height, PixelFormat.Format8bppIndexed);
+            Rectangle r2 = new Rectangle(0, 0, g.Width, g.Height);
+            BitmapData d2 = g.LockBits(r2, ImageLockMode.ReadWrite, g.PixelFormat);
+            IntPtr ptr2 = d2.Scan0;
+            byte[] rgb2 = new byte[d2.Stride * g.Height];
+            int skip2 = d2.Stride - g.Width;
+
+            for (int y = 0, counter1 = 0, counter2 = 0; y < g.Height; ++y, counter1 += skip1, counter2 += skip2)
+                for (int x = 0; x < g.Width; ++x, counter1 += 3, counter2 += 1)
+                    rgb2[counter2] = Convert.ToByte(0.299 * (double)rgb1[counter1 + 2] + 0.587 * (double)rgb1[counter1 + 1] + 0.114 * (double)rgb1[counter1]);
+            System.Runtime.InteropServices.Marshal.Copy(rgb2, 0, ptr2, rgb2.Length);
+
+            ColorPalette myPalette = g.Palette;
+            for (int i = 0; i < 256; ++i) myPalette.Entries[i] = Color.FromArgb(i, i, i);
+            g.Palette = myPalette;
+            g.UnlockBits(d2);
+            return g;
+        }
+        private Bitmap GetBin(Bitmap im)
+        {
+            Rectangle r1 = new Rectangle(0, 0, im.Width, im.Height);
+            BitmapData d1 = im.LockBits(r1, ImageLockMode.ReadOnly, im.PixelFormat);
+            IntPtr ptr1 = d1.Scan0;
+            byte[] rgb1 = new byte[d1.Stride * im.Height];
+            int skip1 = d1.Stride - im.Width * 3;
+            System.Runtime.InteropServices.Marshal.Copy(ptr1, rgb1, 0, rgb1.Length);
+            im.UnlockBits(d1);
+
+            Bitmap g = new Bitmap(im.Width, im.Height, PixelFormat.Format8bppIndexed);
+            Rectangle r2 = new Rectangle(0, 0, g.Width, g.Height);
+            BitmapData d2 = g.LockBits(r2, ImageLockMode.ReadWrite, g.PixelFormat);
+            IntPtr ptr2 = d2.Scan0;
+            byte[] rgb2 = new byte[d2.Stride * g.Height];
+            int skip2 = d2.Stride - g.Width;
+
+            for (int i = 0; i < rgb1.Length; ++i)
+                rgb2[i] = Convert.ToByte((rgb1[i] >= 127) ? 255 : 0);
+            System.Runtime.InteropServices.Marshal.Copy(rgb2, 0, ptr2, rgb2.Length);
+            ColorPalette myPalette = g.Palette;
+            for (int i = 0; i < 256; ++i) myPalette.Entries[i] = Color.FromArgb(i, i, i);
+            g.Palette = myPalette;
+            g.UnlockBits(d2);
+            return g;
+        }
+
+        Bitmap image1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Filter = "所有檔案(*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            image1 = (Bitmap)Bitmap.FromStream(myStream);
+                            if (image1.PixelFormat != PixelFormat.Format8bppIndexed)
+                            {
+                                image1 = GetGray(image1);
+                            }
+                            image1 = GetBin(image1);
+                            pictureBox1.Image = image1;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error.Could not read file:" + ex.Message);
+                }
+            }
+        }
+        private Bitmap Con(Bitmap im, int maskSize, yan flag)//flag=1:Dilation  0:Erostion
+        {
+            Rectangle r1 = new Rectangle(0, 0, im.Width, im.Height);
+            BitmapData d1 = im.LockBits(r1, ImageLockMode.ReadOnly, im.PixelFormat);
+            IntPtr ptr1 = d1.Scan0;
+            byte[] rgb1 = new byte[d1.Stride * im.Height];
+            int skip1 = d1.Stride - im.Width;
+            System.Runtime.InteropServices.Marshal.Copy(ptr1, rgb1, 0, rgb1.Length);
+            im.UnlockBits(d1);
+
+            Bitmap g = new Bitmap(im.Width, im.Height, PixelFormat.Format8bppIndexed);
+            Rectangle r2 = new Rectangle(0, 0, g.Width, g.Height);
+            BitmapData d2 = g.LockBits(r2, ImageLockMode.ReadWrite, g.PixelFormat);
+            IntPtr ptr2 = d2.Scan0;
+            byte[] rgb2 = new byte[d2.Stride * g.Height];
+            int skip2 = d2.Stride - g.Width;
+            bool exist = false;
+            for (int y = 0, counter = 0; y < im.Height; ++y, counter += skip1)
+                for (int x = 0; x < im.Width; ++x, counter += 1)
+                {
+                    exist = false;
+                    for (int i = -maskSize / 2; i <= maskSize / 2; ++i)
+                        for (int j = -maskSize / 2; j <= maskSize / 2; ++j)
+                        {
+                            int temp = counter + i * d1.Stride + j;
+                            if (temp >= 0 && temp < rgb1.Length)
+                            {
+                                if (flag==yan.DILATION)
+                                {
+                                    if (rgb1[temp] == 255) exist = true;
+                                }
+                                else
+                                {
+                                    if (rgb1[temp] == 0) exist = true;
+                                }
+                            }
+                        }
+                    if (flag==yan.DILATION)
+                    {
+                        if (exist)
+                            rgb2[counter] = 255;
+                        else
+                            rgb2[counter] = 0;
+                    }
+                    else
+                    {
+                        if (!exist)
+                            rgb2[counter] = 255;
+                        else
+                            rgb2[counter] = 0;
+                    }
+                }
+
+            System.Runtime.InteropServices.Marshal.Copy(rgb2, 0, ptr2, rgb2.Length);
+            ColorPalette myPalette = g.Palette;
+            for (int i = 0; i < 256; ++i) myPalette.Entries[i] = Color.FromArgb(i, i, i);
+            g.Palette = myPalette;
+            g.UnlockBits(d2);
+            return g;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int maskSize = Convert.ToInt32(textBox1.Text);
+                if (maskSize <= 0) { MessageBox.Show("MaskSize need bigger than 0."); return; }
+                if (maskSize % 2 == 0) { MessageBox.Show("MaskSize must been odd."); return; }
+                image1 = Con(image1, maskSize, yan.DILATION);
+                pictureBox2.Image = image1;
+            }
+            catch (Exception ex) { MessageBox.Show("Error:" + ex.Message); }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int maskSize = Convert.ToInt32(textBox1.Text);
+                if (maskSize <= 0) { MessageBox.Show("MaskSize need bigger than 0."); return; }
+                if (maskSize % 2 == 0) { MessageBox.Show("MaskSize must been odd."); return; }
+                image1 = Con(image1, maskSize, yan.EROSION);
+                pictureBox2.Image = image1;
+            }
+            catch (Exception ex) { MessageBox.Show("Error:" + ex.Message); }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int maskSize = Convert.ToInt32(textBox1.Text);
+                if (maskSize <= 0) { MessageBox.Show("MaskSize need bigger than 0."); return; }
+                if (maskSize % 2 == 0) { MessageBox.Show("MaskSize must been odd."); return; }
+                image1 = Con(image1, maskSize, yan.EROSION);
+                image1 = Con(image1, maskSize, yan.DILATION);
+                pictureBox2.Image = image1;
+            }
+            catch (Exception ex) { MessageBox.Show("Error:" + ex.Message); }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int maskSize = Convert.ToInt32(textBox1.Text);
+                if (maskSize <= 0) { MessageBox.Show("MaskSize need bigger than 0."); return; }
+                if (maskSize % 2 == 0) { MessageBox.Show("MaskSize must been odd."); return; }
+                image1 = Con(image1, maskSize, yan.DILATION);
+                image1 = Con(image1, maskSize, yan.EROSION);
+                pictureBox2.Image = image1;
+            }
+            catch (Exception ex) { MessageBox.Show("Error:" + ex.Message); }
+        }
+    }
+}
