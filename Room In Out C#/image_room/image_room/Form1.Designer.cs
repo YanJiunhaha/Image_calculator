1a2d7536a1c117900ba80e03d5b4c13ece3bd42a
YanJiun <jiun113241@gmail.com>
2019-01-01 04:05:27 +0800

    new

---
+++ Room In Out C#/image_room/image_room/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace image_room
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 14F);
+            this.button1.Location = new System.Drawing.Point(16, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "開啟檔案";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(21, 85);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(461, 413);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 14F);
+            this.label1.Location = new System.Drawing.Point(16, 58);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input :";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("新細明體", 14F);
+            this.button2.Location = new System.Drawing.Point(719, 4);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(255, 58);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Room";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 14F);
+            this.label2.Location = new System.Drawing.Point(504, 58);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Select Range :";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(509, 85);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(461, 413);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 14F);
+            this.label3.Location = new System.Drawing.Point(16, 509);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Output : First-Order";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 14F);
+            this.label4.Location = new System.Drawing.Point(504, 509);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(282, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Output : Bilinear Interpolation";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(21, 535);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(461, 413);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox4.Location = new System.Drawing.Point(509, 535);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(461, 413);
+            this.pictureBox4.TabIndex = 9;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label5.Location = new System.Drawing.Point(152, 24);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "X : ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.textBox1.Location = new System.Drawing.Point(202, 16);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(48, 31);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Text = "0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("新細明體", 12F);
+            this.textBox2.Location = new System.Drawing.Point(311, 16);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(48, 31);
+            this.textBox2.TabIndex = 14;
+            this.textBox2.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label6.Location = new System.Drawing.Point(262, 24);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Y : ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("新細明體", 12F);
+            this.textBox3.Location = new System.Drawing.Point(417, 16);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(48, 31);
+            this.textBox3.TabIndex = 16;
+            this.textBox3.Text = "100";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label7.Location = new System.Drawing.Point(368, 24);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 20);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "長 : ";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("新細明體", 12F);
+            this.textBox4.Location = new System.Drawing.Point(523, 15);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(48, 31);
+            this.textBox4.TabIndex = 18;
+            this.textBox4.Text = "100";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label8.Location = new System.Drawing.Point(473, 22);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 20);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "寬 : ";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("新細明體", 12F);
+            this.textBox5.Location = new System.Drawing.Point(648, 15);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(48, 31);
+            this.textBox5.TabIndex = 20;
+            this.textBox5.Text = "2";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label9.Location = new System.Drawing.Point(589, 22);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(64, 20);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "倍率 : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(987, 960);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "四子四丙_1104105308_顏君翰";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Room In Out C#/image_room/image_room/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace image_room
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Bitmap GetGray(Bitmap im)
+        {
+            Rectangle r1 = new Rectangle(0, 0, im.Width, im.Height);
+            BitmapData d1 = im.LockBits(r1, ImageLockMode.ReadOnly, im.PixelFormat);
+            IntPtr ptr1 = d1.Scan0;
+            byte[] rgb1 = new byte[d1.Stride * im.Height];
+            int skip1 = d1.Stride - im.Width * 3;
+            System.Runtime.InteropServices.Marshal.Copy(ptr1, rgb1, 0, rgb1.Length);
+            im.UnlockBits(d1);
+
+            Bitmap g = new Bitmap(im.Width, im.Height, PixelFormat.Format8bppIndexed);
+            Rectangle r2 = new Rectangle(0, 0, g.Width, g.Height);
+            BitmapData d2 = g.LockBits(r2, ImageLockMode.ReadWrite, g.PixelFormat);
+            IntPtr ptr2 = d2.Scan0;
+            byte[] rgb2 = new byte[d2.Stride * g.Height];
+            int skip2 = d2.Stride - g.Width;
+
+            for (int y = 0, counter1 = 0, counter2 = 0; y < g.Height; ++y, counter1 += skip1, counter2 += skip2)
+                for (int x = 0; x < g.Width; ++x, counter1 += 3, counter2 += 1)
+                    rgb2[counter2] = Convert.ToByte(0.299 * (double)rgb1[counter1 + 2] + 0.587 * (double)rgb1[counter1 + 1] + 0.114 * (double)rgb1[counter1]);
+            System.Runtime.InteropServices.Marshal.Copy(rgb2, 0, ptr2, rgb2.Length);
+
+            ColorPalette myPalette = g.Palette;
+            for (int i = 0; i < 256; ++i) myPalette.Entries[i] = Color.FromArgb(i, i, i);
+            g.Palette = myPalette;
+            g.UnlockBits(d2);
+            return g;
+        }
+        
+        Bitmap ImageRes, Image1;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "所有檔案(*.*)|*.*";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if((myStream = openFileDialog.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            ImageRes = (Bitmap)Bitmap.FromStream(myStream);
+                            if(ImageRes.PixelFormat != PixelFormat.Format8bppIndexed)
+                            {
+                                Image1 = GetGray(ImageRes);
+                            }
+                            else
+                            {
+                                Image1 = ImageRes;
+                            }
+                            pictureBox1.Image = Image1;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Can't open file" + ex.Message);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int position_x = Convert.ToInt32(textBox1.Text);
+                int position_y = Convert.ToInt32(textBox2.Text);
+                int height = Convert.ToInt32(textBox3.Text);
+                if (height > (Image1.Height - position_y)) { height = Image1.Height - position_y; MessageBox.Show("height too bigger"); }
+                int width = Convert.ToInt32(textBox4.Text);
+                if(width > (Image1.Width - position_x)) { width = Image1.Width - position_x; MessageBox.Show("width too bigger"); }
+                double ratio = Convert.ToDouble(textBox5.Text);
+
+                Rectangle r1 = new Rectangle(0, 0, Image1.Width, Image1.Height);
+                BitmapData d1 = Image1.LockBits(r1, ImageLockMode.ReadOnly, Image1.PixelFormat);
+                IntPtr ptr1 = d1.Scan0;
+                byte[] rgb1 = new byte[d1.Stride * Image1.Height];
+                int skip1 = d1.Stride - Image1.Width;
+                System.Runtime.InteropServices.Marshal.Copy(ptr1, rgb1, 0, rgb1.Length);
+                Image1.UnlockBits(d1);
+
+                Bitmap s = new Bitmap(width, height, PixelFormat.Format8bppIndexed);
+                Rectangle r2 = new Rectangle(0, 0, s.Width, s.Height);
+                BitmapData d2 = s.LockBits(r2, ImageLockMode.ReadWrite, s.PixelFormat);
+                IntPtr ptr2 = d2.Scan0;
+                byte[] rgb2 = new byte[d2.Stride * s.Height];
+                int skip2 = d2.Stride - s.Width;
+                
+                for(int y = 0,counter = 0; y < s.Height; ++y, counter+=skip2)
+                {
+                    for(int x = 0; x < s.Width; ++x, ++counter)
+                    {
+                        rgb2[counter] = rgb1[(position_y + y) * d1.Stride + position_x + x];
+                    }
+                }
+                System.Runtime.InteropServices.Marshal.Copy(rgb2, 0, ptr2, rgb2.Length);
+                ColorPalette myPalette = s.Palette;
+                for (int i = 0; i < 256; ++i) myPalette.Entries[i] = Color.FromArgb(i, i, i);
+                s.Palette = myPalette;
+                s.UnlockBits(d2);
+                pictureBox2.Image = s;
+
+                //frist order
+                Bitmap first_img = new Bitmap(Convert.ToInt32(s.Width * ratio),Convert.ToInt32(s.Height*ratio),s.PixelFormat);
+                Rectangle r3 = new Rectangle(0, 0, first_img.Width, first_img.Height);
+                BitmapData d3 = first_img.LockBits(r3, ImageLockMode.ReadWrite, first_img.PixelFormat);
+                IntPtr ptr3 = d3.Scan0;
+                byte[] rgb3 = new byte[d3.Stride * first_img.Height];
+                int skip3 = d3.Stride - first_img.Width;
+
+                for (int y = 0, counter = 0; y < first_img.Height; ++y,counter += skip3)
+                {
+                    for(int x = 0; x<first_img.Width; ++x, ++counter)
+                    {
+                        int h = Convert.ToInt32(y / ratio);
+                        int w = Convert.ToInt32(x / ratio);
+                        int r = Convert.ToInt32(y % ratio);
+                        if (h<(s.Height-1) && w<(s.Width-1))
+                        {
+                            rgb3[counter] = Convert.ToByte((rgb2[h*d2.Stride+w] * (ratio - r) + rgb2[h * d2.Stride + w +1] * r) / ratio);
+                        }
+                    }
+                }
+                System.Runtime.InteropServices.Marshal.Copy(rgb3, 0, ptr3, rgb3.Length);
+                first_img.Palette = myPalette;
+                first_img.UnlockBits(d3);
+                pictureBox3.Image = first_img;
+
+                //bilinear
+                Bitmap bi_img = new Bitmap(Convert.ToInt32(s.Width * ratio), Convert.ToInt32(s.Height * ratio), s.PixelFormat);
+                Rectangle r4 = new Rectangle(0, 0, bi_img.Width, bi_img.Height);
+                BitmapData d4 = bi_img.LockBits(r4, ImageLockMode.ReadWrite, bi_img.PixelFormat);
+                IntPtr ptr4 = d4.Scan0;
+                byte[] rgb4 = new byte[d4.Stride * bi_img.Height];
+                int skip4 = d4.Stride - bi_img.Width;
+
+                for (int y = 0, counter = 0; y < bi_img.Height; ++y, counter += skip4)
+                {
+                    for (int x = 0; x < bi_img.Width; ++x, ++counter)
+                    {
+                        double fh = y / ratio;
+                        int h = Convert.ToInt32(fh);
+                        double fw = x / ratio;
+                        int w = Convert.ToInt32(fw);
+                        if (h < (s.Height - 1) && w < (s.Width - 1))
+                        {
+                            double q11 = rgb2[h*d2.Stride+w];
+                            double q21 = rgb2[h * d2.Stride + w + 1];
+                            double q12 = rgb2[(h + 1) * d2.Stride + w];
+                            double q22 = rgb2[(h + 1) * d2.Stride + w + 1];
+
+                            double R1 = q11 * (w + 1 - fw) + q21 * (fw - w);
+                            double R2 = q12 * (w + 1 - fw) + q22 * (fw - w);
+                            double n = R1 * (h + 1 - fh) + R2 * (fh - h);
+                            if (n > 255) n = 255;
+                            else if (n<0)n = 0;
+                            rgb4[counter] = Convert.ToByte(n);
+
+                        }
+                    }
+                }
+                System.Runtime.InteropServices.Marshal.Copy(rgb4, 0, ptr4, rgb4.Length);
+                bi_img.Palette = myPalette;
+                bi_img.UnlockBits(d4);
+                pictureBox4.Image = bi_img;
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error:" + ex.Message);
+            }
+        }
+    }
+}
